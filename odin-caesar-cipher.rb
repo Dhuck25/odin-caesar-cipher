276a1ded93ef31a0592e8971da36431b8961ea20
David Huck <david.huck25@gmail.com>
2022-03-26 13:56:19 -0400

    Modified odin-caesar-cipher.rb

--- odin-caesar-cipher.rb
+++ odin-caesar-cipher.rb
@@ -14,15 +14,17 @@ def caesar_cipher(str, shift = 1)
         elsif 
     # Check if there is a uppercase letter, return back the shifted uppercase letter
             old_idx = alphabet_up.find_index(letter)
-            new_idx = ( old_idx + shift ) % alphabet_up.count
+    # Modulo checks if the shift will put letter passed "Z", if so it wraps back to "A"
+            new_idx = ( old_idx + shift ) % alphabet_up.count 
             caesar += alphabet_up[new_idx]       
         else
     # Check if there is a lowercase letter, return back the shifted lowercase letter
             old_idx = alphabet_low.find_index(letter)
-            new_idx = ( old_idx + shift )  % alphabet_low.count
+            new_idx = ( old_idx + shift ) % alphabet_low.count
             caesar += alphabet_low[new_idx]
         end
     end
     
     caesar
 end
+
